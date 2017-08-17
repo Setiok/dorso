d51eb44102448743eaf5ede384eb8b60bee94f9a
Setiok <dorso.stjo@laposte.net>
2017-08-17 16:15:08 +0200

    version apres fin de la 1ere heure de taf

---
+++ header.php
@@ -0,0 +1,40 @@
+<div id="header">
+    <div id="logo">
+        <a href="/">
+            <img class="logo" src="./src/logo.jpg">
+        </a>
+        <h1>
+            <span> *Nom du site </span>
+            <br> *objectif du site
+        </h1>
+    </div>
+
+    <ul id="menu">
+        <li>
+            <a href="/">Accueil</a>
+        </li>
+        <li>
+            <a href="#">111111111</a>
+            <ul class="lvl2">
+                <li>
+                    <a href="#">........</a>
+                </li>
+                <li>
+                    <a href="#">........</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a href="#">222222222</a>
+            <ul class="lvl2">
+                <li>
+                    <a href="#">........</a>
+                </li>
+                <li>
+                    <a href="#">........</a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+
+</div>
---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title>Mon site</title>
+
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/header.css">
+</head>
+<body>
+<?php include 'header.php' ?>
+</body>
+</html>
